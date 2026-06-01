18c3076458599fad9f19421eb3b4d5144d663f1a
RaulOAlves <raul.oliveira.alves@hotmail.com>
2026-06-01 17:23:55 -0300

    [FEAT] adiciona keybindings vim com bubbles/key

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module farm-idle
 
 go 1.26.2
+
+require github.com/charmbracelet/bubbles v1.0.0
---
+++ internal/input/keys.go
@@ -0,0 +1,44 @@
+package input
+
+import "github.com/charmbracelet/bubbles/key"
+
+type KeyMap struct {
+	Up      key.Binding
+	Down    key.Binding
+	Select  key.Binding
+	Back    key.Binding
+	Action1 key.Binding
+	Action2 key.Binding
+	Action3 key.Binding
+	Action4 key.Binding
+	Action5 key.Binding
+	Quit    key.Binding
+}
+
+var DefaultKeyMap = KeyMap{
+	Up: key.NewBinding(
+		key.WithKeys("k", "up"),
+		key.WithHelp("k/↑", "cima"),
+	),
+	Down: key.NewBinding(
+		key.WithKeys("j", "down"),
+		key.WithHelp("j/↓", "baixo"),
+	),
+	Select: key.NewBinding(
+		key.WithKeys("l", "enter"),
+		key.WithHelp("l/enter", "confirmar"),
+	),
+	Back: key.NewBinding(
+		key.WithKeys("h", "esc"),
+		key.WithHelp("h/esc", "cancelar"),
+	),
+	Action1: key.NewBinding(key.WithKeys("1")),
+	Action2: key.NewBinding(key.WithKeys("2")),
+	Action3: key.NewBinding(key.WithKeys("3")),
+	Action4: key.NewBinding(key.WithKeys("4")),
+	Action5: key.NewBinding(key.WithKeys("5")),
+	Quit: key.NewBinding(
+		key.WithKeys("q"),
+		key.WithHelp("q", "salvar e sair"),
+	),
+}
